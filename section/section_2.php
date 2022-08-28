53637c06506e0c75ec09b378e1f0dba1ea0a24a9
rizkyefrian2706 <rizkyefrian2706@gmail.com>
2022-08-28 08:05:38 +0700

    update

--- section/section_2.php
+++ section/section_2.php
@@ -1,117 +1,116 @@
 <div class="resume-section-content">
-                <h2 class="mb-5">PROFIL RESPONDEN</h2>
-                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                    <div class="flex-grow-1">
-                        <p class="lead mb-0">1. Berapa usia I/B/S saat ini?</p>
-                        <div class="table-responsive">
-                            <table class="table" border="1">
-                                <tr>
-                                    <td>1</td>
-                                    <td><input type="radio" name="usia" value="Gen Z">
-                                        17 tahun (menikah) - 23 tahun (Gen Z)</td>
-                                    <td>3</td>
-                                    <td><input type="radio" name="usia" value="Gen X"> 40 - 55 tahun (Gen
-                                        X)</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td><input type="radio" name="usia" value="Millenial"> 24 - 39 tahun
-                                        (Millenial)</td>
-                                    <td>4</td>
-                                    <td><input type="radio" name="usia" value="Baby Boomers"> > 56 tahun
-                                        (Baby Boomers)</td>
-                                </tr>
-                            </table>
-                        </div>
-                        <p class="lead mb-0">2. Apa pendidikan terakhir I/B/S?</p>
-                        <div class="table-responsive">
-                            <table class="table" border="1">
-                                <tr>
-                                    <td>1</td>
-                                    <td><input type="radio" name="pendidikan" value="Tidak Sekolah"> Tidak Sekolah</td>
-                                    <td>3</td>
-                                    <td><input type="radio" name="pendidikan" value="SMP"> SMP</td>
-                                    <td>5</td>
-                                    <td><input type="radio" name="pendidikan" value="Sarjana (D3/S1)"> Sarjana (D3/S1)</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td><input type="radio" name="pendidikan" value="SD"> SD</td>
-                                    <td>4</td>
-                                    <td> <input type="radio" name="pendidikan" value="SMA"> SMA</td>
-                                    <td>6</td>
-                                    <td> <input type="radio" name="pendidikan" value="Pascasarjana (S2/S3)"> Pascasarjana (S2/S3)</td>
-                                </tr>
-                            </table>
-                        </div>
-                        <p class="lead mb-0">3. Berapa pendapatan bersih individu I/B/S (rata-rata dalam 1 bulan)?</p>
-                        <div class="table-responsive">
-                            <table class="table" border="1">
-                                <tr>
-                                    <td>1</td>
-                                    <td><input type="radio" name="pendapatan" value="< 2.000.000">
-                                        < Rp.2.000.000</td>
-                                    <td>3</td>
-                                    <td><input type="radio" name="pendapatan" value="3.000.000 - 4.000.000"> Rp.3.000.000 - Rp.4.000.000</td>
-                                    <td>5</td>
-                                    <td><input type="radio" name="pendapatan" value="6.000.000 - 8.000.000"> Rp.6.000.000 - Rp.8.000.000</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td><input type="radio" name="pendapatan" value="2.000.000 - 3.000.000"> Rp.2.000.000 - Rp.3.000.000 </td>
-                                    <td>4</td>
-                                    <td><input type="radio" name="pendapatan" value="4.000.000 - 6.000.000"> Rp.4.000.000 - Rp.6.000.000</td>
-                                    <td>6</td>
-                                    <td> <input type="radio" name="pendapatan" value="> 8.000.000"> > Rp.8.000.000</td>
-                                </tr>
-                            </table>
-                        </div>
-                        <p class="lead mb-0">4. Apa pekerjaan (profesi) I/B/S?</p>
-                        <div class="table-responsive">
-                            <table class="table" border="1">
-                                <tr>
-                                    <td>1.<input type="radio" name="profesi" value="ASN/PNS"> ASN/PNS </td>
-                                    <td>7.<input type="radio" name="profesi" value="TNI/Polri"> TNI/Polri</td>
-                                </tr>
-                                <tr>
-                                    <td>2.<input type="radio" name="profesi" value="Ibu Rumah Tangga/Bapak Rumah
-                                        Tangga"> Ibu Rumah Tangga/Bapak Rumah
-                                        Tangga</td>
-                                    <td>8.<input type="radio" name="profesi" value="Professional
-                                        (Pengacara/Konsultan/Dokter)"> Professional
-                                        (Pengacara/Konsultan/Dokter)</td>
-                                </tr>
-                                <tr>
-                                    <td>3.<input type="radio" name="profesi" value="Pedagang/Wirausaha"> Pedagang/Wirausaha</td>
-                                    <td>9.<input type="radio" name="profesi" value="Tenaga Kesehatan (Perawat/Bidan)"> Tenaga Kesehatan (Perawat/Bidan)
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>4.<input type="radio" name="profesi" value="Pegawai Swasta"> Pegawai Swasta</td>
-                                    <td>10.<input type="radio" name="profesi" value="Tenaga Pengajar (Guru/Dosen)"> Tenaga Pengajar (Guru/Dosen)
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>5.<input type="radio" name="profesi" value="Pelajar/Mahasiswa"> Pelajar/Mahasiswa</td>
-                                    <td>11.<input type="radio" name="profesi" value="Tidak Bekerja (Pengangguran)"> Tidak Bekerja (Pengangguran)
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>6.<input type="radio" name="profesi" value="Karyawan BUMN/BUMD"> Karyawan BUMN/BUMD</td>
-                                    <td><div style="display: flex;">99.<input type="radio" name="profesi" value="other"> 
-                                    <div><input type="text" id="other" placeholder="Lainnya..."
-                                    class="form-control"></div>
-                                    </div>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-12"><button class="btn btn-primary" onclick="save_profile()"
-                                    style="color:white; width:100%">Simpan</button></div>
-                        </div>
-                    </div>
-                    <div class="flex-shrink-0"><span class="text-primary"></span></div>
-                </div>
+    <h2 class="mb-5">PROFIL RESPONDEN</h2>
+    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div class="flex-grow-1">
+            <p class="lead mb-0">1. Berapa usia I/B/S saat ini?</p>
+            <div class="table-responsive">
+                <table class="table" border="1">
+                    <tr>
+                        <td>1</td>
+                        <td><input type="radio" name="usia" value="Gen Z">
+                            17 tahun (menikah) - 23 tahun (Gen Z)</td>
+                        <td>3</td>
+                        <td><input type="radio" name="usia" value="Gen X"> 40 - 55 tahun (Gen
+                            X)</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td><input type="radio" name="usia" value="Millenial"> 24 - 39 tahun
+                            (Millenial)</td>
+                        <td>4</td>
+                        <td><input type="radio" name="usia" value="Baby Boomers"> > 56 tahun
+                            (Baby Boomers)</td>
+                    </tr>
+                </table>
             </div>
+            <p class="lead mb-0">2. Apa pendidikan terakhir I/B/S?</p>
+            <div class="table-responsive">
+                <table class="table" border="1">
+                    <tr>
+                        <td>1</td>
+                        <td><input type="radio" name="pendidikan" value="Tidak Sekolah"> Tidak Sekolah</td>
+                        <td>3</td>
+                        <td><input type="radio" name="pendidikan" value="SMP"> SMP</td>
+                        <td>5</td>
+                        <td><input type="radio" name="pendidikan" value="Sarjana (D3/S1)"> Sarjana (D3/S1)</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td><input type="radio" name="pendidikan" value="SD"> SD</td>
+                        <td>4</td>
+                        <td> <input type="radio" name="pendidikan" value="SMA"> SMA</td>
+                        <td>6</td>
+                        <td> <input type="radio" name="pendidikan" value="Pascasarjana (S2/S3)"> Pascasarjana (S2/S3)</td>
+                    </tr>
+                </table>
+            </div>
+            <p class="lead mb-0">3. Berapa pendapatan bersih individu I/B/S (rata-rata dalam 1 bulan)?</p>
+            <div class="table-responsive">
+                <table class="table" border="1">
+                    <tr>
+                        <td>1</td>
+                        <td><input type="radio" name="pendapatan" value="< 2.000.000">
+                            < Rp.2.000.000</td>
+                        <td>3</td>
+                        <td><input type="radio" name="pendapatan" value="3.000.000 - 4.000.000"> Rp.3.000.000 - Rp.4.000.000</td>
+                        <td>5</td>
+                        <td><input type="radio" name="pendapatan" value="6.000.000 - 8.000.000"> Rp.6.000.000 - Rp.8.000.000</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td><input type="radio" name="pendapatan" value="2.000.000 - 3.000.000"> Rp.2.000.000 - Rp.3.000.000 </td>
+                        <td>4</td>
+                        <td><input type="radio" name="pendapatan" value="4.000.000 - 6.000.000"> Rp.4.000.000 - Rp.6.000.000</td>
+                        <td>6</td>
+                        <td> <input type="radio" name="pendapatan" value="> 8.000.000"> > Rp.8.000.000</td>
+                    </tr>
+                </table>
+            </div>
+            <p class="lead mb-0">4. Apa pekerjaan (profesi) I/B/S?</p>
+            <div class="table-responsive">
+                <table class="table" border="1">
+                    <tr>
+                        <td>1.<input type="radio" name="profesi" value="ASN/PNS"> ASN/PNS </td>
+                        <td>7.<input type="radio" name="profesi" value="TNI/Polri"> TNI/Polri</td>
+                    </tr>
+                    <tr>
+                        <td>2.<input type="radio" name="profesi" value="Ibu Rumah Tangga/Bapak Rumah
+                                        Tangga"> Ibu Rumah Tangga/Bapak Rumah
+                            Tangga</td>
+                        <td>8.<input type="radio" name="profesi" value="Professional
+                                        (Pengacara/Konsultan/Dokter)"> Professional
+                            (Pengacara/Konsultan/Dokter)</td>
+                    </tr>
+                    <tr>
+                        <td>3.<input type="radio" name="profesi" value="Pedagang/Wirausaha"> Pedagang/Wirausaha</td>
+                        <td>9.<input type="radio" name="profesi" value="Tenaga Kesehatan (Perawat/Bidan)"> Tenaga Kesehatan (Perawat/Bidan)
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>4.<input type="radio" name="profesi" value="Pegawai Swasta"> Pegawai Swasta</td>
+                        <td>10.<input type="radio" name="profesi" value="Tenaga Pengajar (Guru/Dosen)"> Tenaga Pengajar (Guru/Dosen)
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>5.<input type="radio" name="profesi" value="Pelajar/Mahasiswa"> Pelajar/Mahasiswa</td>
+                        <td>11.<input type="radio" name="profesi" value="Tidak Bekerja (Pengangguran)"> Tidak Bekerja (Pengangguran)
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>6.<input type="radio" name="profesi" value="Karyawan BUMN/BUMD"> Karyawan BUMN/BUMD</td>
+                        <td style="display: flex; align-items: center;">
+                            99. <input type="radio" name="profesi" value="other"> Lainnya:
+                                <div><input type="text" id="other" placeholder="............................" class="form-control"></div>
+                            
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-12"><button class="btn btn-primary" onclick="save_profile()" style="color:white; width:100%">Simpan</button></div>
+            </div>
+        </div>
+        <div class="flex-shrink-0"><span class="text-primary"></span></div>
+    </div>
+</div>
